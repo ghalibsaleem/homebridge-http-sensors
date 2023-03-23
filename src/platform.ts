aeb7734dc0809e31f1d8a7bf17094e09a7a73e92
Ghalib Saleem <ghalib.saleem.123@gmail.com>
2023-03-23 04:44:41 +0000

    Another

--- src/platform.ts
+++ src/platform.ts
@@ -85,7 +85,7 @@ export class HomebridgeHttpSensonrs implements DynamicPlatformPlugin {
 
         // create the accessory handler for the restored accessory
         // this is imported from `platformAccessory.ts`
-        new EnvironmentSensorPlatformAccessory(this, existingAccessory, this.config);
+        new EnvironmentSensorPlatformAccessory(this, existingAccessory, this.config.sensors);
 
         // it is possible to remove platform accessories at any time using `api.unregisterPlatformAccessories`, eg.:
         // remove platform accessories when no longer present
@@ -104,7 +104,7 @@ export class HomebridgeHttpSensonrs implements DynamicPlatformPlugin {
 
         // create the accessory handler for the newly create accessory
         // this is imported from `platformAccessory.ts`
-        new EnvironmentSensorPlatformAccessory(this, accessory, this.config);
+        new EnvironmentSensorPlatformAccessory(this, accessory, this.config.sensors);
 
         // link the accessory to your platform
         this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);

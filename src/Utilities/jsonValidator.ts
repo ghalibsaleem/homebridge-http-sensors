8aa8fa782b2c13892d23a208995156ed624b7376
Ghalib Saleem <ghalib.saleem.123@gmail.com>
2023-03-24 21:37:28 +0000

    Updated the schema draft

--- src/Utilities/jsonValidator.ts
+++ src/Utilities/jsonValidator.ts
@@ -3,9 +3,12 @@ import * as schema from '../config/schema.json';
 
 
 
+
 const ajv = new Ajv();
 
 export function validateConfig(input) {
+  //import foo = require('ajv/lib/refs/json-schema-draft-04.json');
+  //ajv.addMetaSchema(foo);
   const isValid = ajv.validate(schema, input);
   if (!isValid) {
     return undefined;
